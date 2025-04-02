a1afff175fd6233834e6a48488713e84361f67db
anmolsingh7275 <anmolprogramming@gmail.com>
2025-04-03 02:40:05 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log(" node Project");
+const express = require("express")
+const app = express();
+
+// app.use( "/",(req,res)=>{
+//     res.send("hello  from dashboard");
+// })
+
+app.use( "/hello",(req,res)=>{
+    res.send("hello  hello!");
+})
+
+app.use( "/test",(req,res)=>{
+    res.send("hello from the server!");
+})
+app.listen(3000 , ()=>{
+    console.log("server is sucessfull running on port 3000");
+});
